5379fac063c975a0b2e95b10389a19831b0cb270
imhsart <nitinrana2519@gmail.com>
2026-05-25 16:45:09 +0530

    Assignment attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [input, setInput] = useState('')
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+        {input && <p>Hello, {input}!</p>}
     </div>
   )
 }
